c0ed8f29346350a69427b28c00817763acb75a17
soflarity <55975006+soflarity@users.noreply.github.com>
2021-07-06 15:43:10 +0100

    DB integration and test search query implatentation

--- app.js
+++ app.js
@@ -8,8 +8,9 @@ const { MongoClient } = require('mongodb');
 
 //Middleware 
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+app.use(bodyParser.json());
 
 // Database Connection
 // const uri = process.env.MONGO_DB_URI;
@@ -23,15 +24,19 @@ const db = client.db("hubLockerDB");// perform actions on the collection object
 
 
 // Routes
-app.get('/api', (req, res) => {
-    db.collection('country').find().toArray(function (err, docs) {
-        err ? console.log(err) : res.status(201).send({ status: 'Successful', docs });
-    })
-});
+// app.get('/api', (req, res) => {
+//     db.collection('country').find().toArray((err, docs) => {
+//         err ? console.log(err) : res.status(201).send({ status: 'Successful', docs });
+//     })
+// });
 
-app.post('/api/search', (req, res) => {
-    let data = req.body;
-    res.status(201).send({ message: 'Received locker query', body: data });
+app.get('/api/search/:query', (req, res) => {
+    let data = req.params.query;
+    db.collection('country').find({ name: data }).toArray((err, result) => {
+        console.log(result);
+        err ? console.log(err) : res.status(201).send(result[0])
+    })
+    // res.status(201).send({ message: 'Received locker query', body: data });
 })
 
 
